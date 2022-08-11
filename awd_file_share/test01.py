ce769d37785c7e17ceb869b57c9721adb7524f15
1651515568 <1651515568@qq.com>
2022-08-11 17:25:43 +0800

    v0.1.1

--- awd_file_share/test01.py
+++ awd_file_share/test01.py
@@ -8,26 +8,32 @@ def show_file():
     file_list = os.listdir(PATH + '/share_file')
     file_down = req.args.get('flag')#get数据
     file_read = req.args.get('read')#get数据
+    #f = req.files['file']
+
     # file_name = req.values.get('flag')#所有数据
     if file_down:
-        print(file_down)
+        print('down',file_down)
         return send_file(PATH + '/share_file/{}'.format(file_down),as_attachment=True)#,attachment_filename='b.txt'
     elif file_read:
-        print(file_read)
-        return send_file(PATH + '/share_file/{}'.format(file_read),as_attachment=False)#,attachment_filename='b.txt'
+        try:
+            print('read',file_read)
+            can_konw = ['html','txt','jpg','png','art','au','aiff','xbm']
+            if file_read.split('.')[-1].lower() in can_konw:
+                return send_file(PATH + '/share_file/{}'.format(file_read),as_attachment=False)#,attachment_filename='b.txt'
+            else:
+                tmp_file = []
+                with open(PATH + '/share_file/{}'.format(file_read), 'r',encoding='utf-8') as f:
+                    for i in f.readlines():
+                        tmp_file.append(i)
+                return render_template('show.html',file = tmp_file)
+        except Exception as e:
+            return 'can\'t open this file!!!'
+    elif req.method == 'POST' and req.values.get('upload') == '提交':
+        file = req.files['upload']
+        the_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime()) 
+        file.save("{}/share_file/up-{}-{}".format(PATH,the_time,file.filename))
+        return render_template('index.html',file = file_list)
     else:
         return render_template('index.html',file = file_list)
-@app.route('/downfile.html',methods=['GET','POST'])
-def get_date_for_qq_robot():
-    # msg = req.get_data()
-    file_tag = req.args.get('flag')
-    file_name = req.values.get('flag')
-
-    if file_name == 'a':
-        
-        # return send_file('E:/work-code\python-code\ctf/a.txt',mimetype=None,as_attachment=True,attachment_filename='b.txt')
-        return 'dwadawdaw'
-    else:
-        return 'fwafawd'
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
