65f83e5ecaa613154e02062c4416f4470a402459
1651515568 <1651515568@qq.com>
2022-08-16 15:34:12 +0800

    php批量文件包含

---
+++ awd_tools/write_config_all.php
@@ -0,0 +1,39 @@
+<?php
+function write_file_php($file){
+    $code = "<?php include('/var/www/html/watchbird/watchbird.php'); ?>\n";
+    $code = $code.file_get_contents($file);
+    file_put_contents($file,$code);
+}
+function getDir($path){
+    $pass_array = array('/var/www/html/watchbird/watchbird.php','/var/www/html/write_config_all.php');
+    if(!file_exists($path)){
+        return [];
+    }
+    $handel = dir($path);
+    $fileItem = [];
+    if($handel){
+        while(($file = $handel->read()) !== false){
+            $newPath = $path . DIRECTORY_SEPARATOR . $file;
+            $f = explode('.',$newPath);
+            $f = end($f);
+            if(is_dir($newPath) && $file != '.' && $file != '..'){
+                getDir($newPath);
+            }/**else if(is_file($newPath)) {
+             //   echo $newPath.'<br>';
+            }**/
+            if($f == 'php'){
+                if(in_array($newPath,$pass_array,TRUE)){
+                    $status = 'pass';
+                }else{
+                    echo $newPath.'<br>';
+                    write_file_php($newPath);
+                }
+            }
+        }
+    }
+    $handel->close();
+}
+echo('开始写入配置...<br>');
+$path = realpath('.');
+getDir($path);
+echo('配置写入完毕!<br>');
